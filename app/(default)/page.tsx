09a5d4a3c29ff1b4d163b8de8f5e0ab410732b76
Guilherme Medeiros <guilhermedeamorimmedeiros@yahoo.com.br>
2023-11-20 17:41:55 -0300

    Readme update

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,14 +1,13 @@
 export const metadata = {
   title: 'Constellation Corretora',
-  description: 'Page description',
+  description: 'Empresa especializada em seguros diversos, atuando no mercado securitário a mais de uma década. Nosso compromisso é proteger o que é mais importante para você!',
 }
 
 import Hero from '@/components/hero'
 import Features from '@/components/features'
-import Newsletter from '@/components/newsletter'
 import Zigzag from '@/components/zigzag'
 import Testimonials from '@/components/testimonials'
-import Forms from '@/components/forms'
+
 
 export default function Home() {
   return (
@@ -17,7 +16,6 @@ export default function Home() {
       <Features />
       <Zigzag />
       <Testimonials />
-      <Newsletter />
     </>
   )
 }
